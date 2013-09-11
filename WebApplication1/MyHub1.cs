f19f2756bb47506bd2b365471172485a293a8bf3
jsakamoto <junichi.jp@gmail.com>
2013-09-11 19:35:39 +0900

    1st commit.

---
+++ WebApplication1/MyHub1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication1
+{
+    public class MyHub1 : Hub
+    {
+        public void ChangeState(bool newState)
+        {
+            this.Clients.All.StateChanged(newState);
+        }
+    }
+}
